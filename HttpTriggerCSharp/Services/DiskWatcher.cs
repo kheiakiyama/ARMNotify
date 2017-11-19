064079a89d576f368e503b903988f93d168e8362
kheiakiyama <kheiakiyama@gmail.com>
2017-11-19 16:08:34 +0900

    list up not attached disk

---
+++ HttpTriggerCSharp/Services/DiskWatcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.Compute.Fluent;
+namespace AzureResourcesWasteNotice
+{
+    public class DiskWatcher : IAzureResourceWatcher
+    {
+        public DiskWatcher()
+        {
+        }
+
+        public AzureResource[] Collect(IAzure azure)
+        {
+            return azure.Disks
+                .List()
+                .WithoutNoProduction()
+                .Where(q => !q.IsAttachedToVirtualMachine)
+                .Select(q => new AzureResource()
+                {
+                    ResourceGroupName = q.ResourceGroupName,
+                    Name = q.Name,
+	                ResourceTypeName = q.Type,
+	                State = q.Sku.ToString(),
+                    ResourceId = q.Id,
+                })
+                .ToArray();
+        }
+    }
+}
