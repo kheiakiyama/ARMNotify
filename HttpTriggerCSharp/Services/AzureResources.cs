9c971e590c4eb04c1e98c55d707942c0eb5ffa65
kheiakiyama <kheiakiyama@gmail.com>
2017-11-25 12:24:53 +0900

    fix comment out

--- HttpTriggerCSharp/Services/AzureResources.cs
+++ HttpTriggerCSharp/Services/AzureResources.cs
@@ -10,10 +10,10 @@ namespace AzureResourcesWasteNotice
         public static IAzureResourceWatcher[] GetFactories()
         {
             return new IAzureResourceWatcher[] {
-                //new VirtualMachineWatcher(),
-                //new AppServiceWatcher(),
-                //new DatabaseWatcher(),
-                //new DiskWatcher(),
+                new VirtualMachineWatcher(),
+                new AppServiceWatcher(),
+                new DatabaseWatcher(),
+                new DiskWatcher(),
                 new ApplicationGatewayWatcher(),
                 new RedisWatcher()
             };
